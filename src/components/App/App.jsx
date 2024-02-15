c18b1a0dd3a97cc6206dd484941f7fb09968746e
Serhii Plis <serzh2577@gmail.com>
2024-02-16 00:53:52 +0300

    adds description

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import Description from '../Description/Description';
+import './App.css';
+
+export default function App() {
+  return (
+    <>
+      <Description />
+    </>
+  );
+}
