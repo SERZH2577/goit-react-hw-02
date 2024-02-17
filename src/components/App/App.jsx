988ffea6ac4517730eae82268a4f9b40a889d1d0
Serhii Plis <serzh2577@gmail.com>
2024-02-18 00:37:45 +0300

    adds Effect

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,30 @@
 import { useState } from 'react';
+import { useEffect } from 'react';
 import Description from '../Description/Description';
 import Feedback from '../Feedback/Feedback';
 import Options from '../Options/Options';
 import './App.css';
 
-export default function App() {
-  const [clicks, setClicks] = useState({
+const getInitialClicks = () => {
+  const storageClicks = localStorage.getItem('clicks');
+
+  if (storageClicks) {
+    return JSON.parse(storageClicks);
+  }
+
+  return {
     good: 0,
     neutral: 0,
     bad: 0,
-  });
+  };
+};
+
+export default function App() {
+  const [clicks, setClicks] = useState(getInitialClicks);
+
+  useEffect(() => {
+    localStorage.setItem('clicks', JSON.stringify(clicks));
+  }, [clicks]);
 
   const handleClick = key => {
     setClicks({ ...clicks, [key]: clicks[key] + 1 });
